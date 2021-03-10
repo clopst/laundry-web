ec9caa133daf55034c5427109403ad1c4fa208ca
Dimas Nurfauzi <dimasnrfzi@gmail.com>
2021-03-10 16:29:31 +0700

    integrate dashboard

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Card, Col, Row, Table, Typography } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row, Spin, Table, Typography } from 'antd';
 import { ShopOutlined, ShoppingOutlined, SkinOutlined, TeamOutlined } from '@ant-design/icons';
 import { currencyFormatter } from '../../helpers/currency';
+import { indexDashboard } from '../../services/dashboard';
+import handleError from '../../helpers/handleError';
+import { PAYMENT_STATUS, TRANSACTION_STATUS } from '../../helpers/const';
+import dayjs from 'dayjs';
 
 const { Title } = Typography;
 
 const Dashboard = (props) => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = () => {
+    setLoading(true);
+    indexDashboard()
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(err => handleError(err))
+      .finally(() => setLoading(false));
+  }
+
   const iconStyle = {
     fontSize: 32
   };
@@ -13,22 +34,22 @@ const Dashboard = (props) => {
   const cards = [
     {
       title: 'Transaksi Hari Ini',
-      amount: 10,
+      amount: data.today_transactions_count ?? 0,
       icon: <ShoppingOutlined style={iconStyle} />
     },
     {
       title: 'Jumlah Customer',
-      amount: 14,
+      amount: data.customers_count ?? 0,
       icon: <TeamOutlined style={iconStyle} />
     },
     {
       title: 'Jumlah Outlet',
-      amount: 3,
+      amount: data.outlets_count ?? 0,
       icon: <ShopOutlined style={iconStyle} />
     },
     {
       title: 'Jumlah Produk',
-      amount: 4,
+      amount: data.products_count ?? 0,
       icon: <SkinOutlined style={iconStyle} />
     }
   ];
@@ -48,80 +69,94 @@ const Dashboard = (props) => {
     </Col>
   ));
 
-  const dataSource = [
-    {
-        id: 1,
-        cashier_id: 3,
-        outlet_id: 1,
-        customer_id: 1,
-        invoice: 'LDB0001',
-        date: '2021-03-01',
-        product_id: 1,
-        qty: 4,
-        totalPrice: 28000,
-        status: 'done',
-        payment: 'done'
+  // const dataSource = [
+  //   {
+  //       id: 1,
+  //       cashier_id: 3,
+  //       outlet_id: 1,
+  //       customer_id: 1,
+  //       invoice: 'LDB0001',
+  //       date: '2021-03-01',
+  //       product_id: 1,
+  //       qty: 4,
+  //       totalPrice: 28000,
+  //       status: 'done',
+  //       payment: 'done'
         
-    },
-    {
-        id: 2,
-        cashier_id: 4,
-        outlet_id: 2,
-        customer_id: 2,
-        invoice: 'LDB0002',
-        date: '2021-03-01',
-        product_id: 2,
-        qty: 1,
-        totalPrice: 35000,
-        status: 'process',
-        payment: 'waiting'
-    },
-  ];
+  //   },
+  //   {
+  //       id: 2,
+  //       cashier_id: 4,
+  //       outlet_id: 2,
+  //       customer_id: 2,
+  //       invoice: 'LDB0002',
+  //       date: '2021-03-01',
+  //       product_id: 2,
+  //       qty: 1,
+  //       totalPrice: 35000,
+  //       status: 'process',
+  //       payment: 'waiting'
+  //   },
+  // ];
   
   const columns = [
     {
+        title: 'Tanggal Transaksi',
+        key: 'date',
+        render: (text, record) => dayjs(record.date).locale('id').format('DD MMMM YYYY')
+    },
+    {
         title: 'Kode Invoice',
         key: 'invoice',
-        dataIndex: 'invoice',
-        sorter: true,
-        sortDirections: ['descend', 'ascend'],
+        dataIndex: 'invoice'
     },
     {
         title: 'Customer',
         key: 'customer',
-        dataIndex: 'customer_id'
+        render: (text, record) => record.customer.name
     },
     {
         title: 'Outlet',
-        key: 'outlet_id',
-        dataIndex: 'outlet_id'
+        key: 'outlet',
+        render: (text, record) => record.outlet.name
     },
     {
         title: 'Produk',
         key: 'product',
-        render: (text, record) => `${record.product_id}  (${record.qty})`
+        render: (text, record) => `${record.product?.name} (${record.qty} ${record.product?.unit})`
     },
     {
         title: 'Harga',
-        key: 'totalPrice',
-        render: (text, record) => currencyFormatter(record.totalPrice)
+        key: 'total_price',
+        render: (text, record) => currencyFormatter(record.total_price)
     },
     {
         title: 'Kasir',
         key: 'cashier',
-        dataIndex: 'cashier_id'
+        render: (text, record) => record.cashier.name
+    },
+    {
+        title: 'Status',
+        key: 'status',
+        render: (text, record) => TRANSACTION_STATUS.find(status => status.value === record.status)?.label
+    },
+    {
+        title: 'Pembayaran',
+        key: 'payment',
+        render: (text, record) => PAYMENT_STATUS.find(status => status.value === record.payment)?.label
     }
   ];
 
   return (
     // <PageBackground noBg>
+    <Spin spinning={loading}>
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
         {renderCards}
         <Col span={24} style={{ marginTop: 16, minHeight: '200px' }}>
-          <Card title="Daftar Transaksi Terbaru">
-            <Card.Meta description="10 Transaksi Terakhir" />
+          <Card title="Transaksi Terbaru">
+            <Card.Meta description="Daftar 10 Transaksi Terakhir" />
             <Table 
-              dataSource={dataSource} 
+              dataSource={data.latest_transactions} 
               columns={columns} 
               rowKey="id" 
               pagination={false} 
@@ -129,6 +164,7 @@ const Dashboard = (props) => {
           </Card>
         </Col>
       </Row>
+    </Spin>
     // </PageBackground>
   );
 }
---
+++ src/services/dashboard.js
@@ -0,0 +1,7 @@
+import { apiGet } from "./api";
+
+const indexDashboard = params => apiGet('/api/dashboard', { params })
+
+export {
+  indexDashboard
+};
